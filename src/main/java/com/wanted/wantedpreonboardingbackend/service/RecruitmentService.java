5bb1040482de0b84477d6e8de2a567b2e7d4f6bd
DESKTOP-CVC0BIM\dxwave <wshan@imcloud.co.kr>
2023-10-13 09:56:39 +0900

    for test

--- src/main/java/com/wanted/wantedpreonboardingbackend/service/RecruitmentService.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/service/RecruitmentService.java
@@ -6,11 +6,13 @@ import com.wanted.wantedpreonboardingbackend.persistence.repository.RecruitmentN
 import com.wanted.wantedpreonboardingbackend.persistence.repository.RecruitmentNoticeRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RecruitmentService {
