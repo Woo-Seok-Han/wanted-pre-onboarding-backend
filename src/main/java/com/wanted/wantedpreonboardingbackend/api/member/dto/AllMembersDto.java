f497299ea40558558744f4830cd47b9be1ea73d8
wshan <woosuk1893@naver.com>
2023-11-01 23:57:54 +0900

    feat : 패키지 구조 추가 및 구성원 조회 api 추가

---
+++ src/main/java/com/wanted/wantedpreonboardingbackend/api/member/dto/AllMembersDto.java
@@ -0,0 +1,12 @@
+package com.wanted.wantedpreonboardingbackend.api.member.dto;
+
+public class AllMembersDto {
+
+    public record Request(
+        Long companyId
+    ){}
+
+    public record Response(
+        
+    ) {}
+}
